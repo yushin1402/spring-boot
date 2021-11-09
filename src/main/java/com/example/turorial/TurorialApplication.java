0418fc509f45a66e821dbf43322807b5a92d45a0
yushin1402 <s.masaomi16h@gmail.com>
2021-11-10 02:13:06 +0900

    first commit

---
+++ src/main/java/com/example/turorial/TurorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.turorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurorialApplication.class, args);
+	}
+
+}
